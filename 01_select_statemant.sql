bc7d2fd8d418a3296596371180fda4ff3f432a00
krushna Gavali <krushnagawali744@gmail.com>
2022-12-10 11:30:27 +0530

    Update 01_select_statemant.sql

--- 01_select_statemant.sql
+++ 01_select_statemant.sql
@@ -9,3 +9,10 @@ thats gonna select all values from emp table
 
 Here, column1, column2, ... 
 are the field names of the table you want to select data from. If you want to select all the fields available in the table
+
+Example:==>>>
+
+1. write a query to display amploye name and salary from emp table
+
+>>  select ename , sal
+    from emp;

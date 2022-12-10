dd64a6313b0c5016c12afea040bc3aa3f424e9c3
krushna Gavali <krushnagawali744@gmail.com>
2022-12-10 11:27:54 +0530

    Update and rename 01_SQL_statemants.sql to 01_select_statemant.sql

--- 01_SQL_statemants.sql
+++
@@ -1,5 +0,0 @@
-SQL Statements:--->>
-statement selects all the records 
-syntax :==>> select * from emp;
-
-thats gonna select all values in emp table
---
+++ 01_select_statemant.sql
@@ -0,0 +1,11 @@
+SQL Statements:--->>
+statement selects all the records 
+syntax :==>> 
+
+SELECT column1, column2, ...
+FROM table_name;
+
+thats gonna select all values from emp table
+
+Here, column1, column2, ... 
+are the field names of the table you want to select data from. If you want to select all the fields available in the table
